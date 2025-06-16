c4e169c46b8f336af9bf8565fd18ccb846d8b356
Trust Jonathan <trustjonathan205@gmail.com>
2025-06-16 12:25:00 +0300

    10

---
+++ main.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+  // alert("ok it's ready");
+  $("#open").click(function () {
+    $("#open").hide();
+    $("#close").show();
+    $(".header-cont header #nav-mobo").css("transform", "translateY(0%)");
+  });
+
+  $("#close").click(function () {
+    $("#close").hide();
+    $("#open").show();
+    $(".header-cont header #nav-mobo").css("transform", "translateY(-100%)");
+  });
+
+  $(".header-cont header #nav-mobo a").click(function () {
+    $("#close").hide();
+    $("#open").show();
+    $(".header-cont header #nav-mobo").css("transform", "translateY(-100%)");
+  });
+});
